0ac3fbbcbf5987c55db8fc1c6043b99e9b1c8498
Gabriel Sousa <gabriel_vms@hotmail.com>
2026-07-29 19:18:21 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: 10000,
+    allowedHosts: ['site-raca-offroad.onrender.com'],
+  },
 })
